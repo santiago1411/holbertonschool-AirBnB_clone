57121db47864f3efc34afc5dae9975c278b5e541
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-07-06 19:17:42 -0500

    Amenety

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
-
-"""
-unittest for user
-"""
-
-import models
-import unittest
-from models.base_model import BaseModel
-from models import state
+'''Unittest for State class'''
 from models.state import State
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
 
 
-class Test_state(unittest.TestCase):
+class TestUserClass(unittest.TestCase):
 
-    """"""
+	def setUp(self):
+		"""Sets up user instance"""
+		self.User_1 = State()
 
-    def setUp(self):
-        """Sets up user instance"""
-        self.my_state = State()
+	def test_type(self):
+		"""test to check type of instance"""
+		self.assertEqual(type(self.User_1), State)
 
-    def test_is_an_instance_user(self):
-        """user instatiating"""
-        my_state = State()
-        self.assertIsInstance(my_state, State)
+	def test_user_id(self):
+		"""test to check user id"""
+		User_1 = State()
+		self.assertEqual(type(User_1.id), str)
+		self.assertTrue(hasattr(User_1, "id"))
 
-    def test_State_id(self):
-        my_state = State()
-        self.assertIsInstance(my_state.id, str)
-        self.assertTrue(hasattr(my_state, "id"))
+	def test_user_created(self):
+		"""test to check user created_at"""
+		User_1 = State()
+		self.assertEqual(type(User_1.created_at), type(datetime.now()))
+		self.assertTrue(hasattr(User_1, "updated_at"))
 
-    def test_created(self):
-        my_state = State()
-        self.assertTrue(type(my_state.created_at), str)
-        self.assertTrue(hasattr(my_state, "created_at"))
+	def test_user_updated(self):
+		"""test to check user updated_at"""
+		User_1 = State()
+		self.assertEqual(type(User_1.updated_at), type(datetime.now()))
+		self.assertTrue(hasattr(User_1, "updated_at"))
 
-    def test_updated(self):
-        my_state = State()
-        self.assertTrue(type(my_state.updated_at), str)
-        self.assertTrue(hasattr(my_state, "updated_at"))
+	def test_user_name(self):
+		"""test to check user name"""
+		User_1 = State()
+		self.assertEqual(type(User_1.name), str)
+		self.assertTrue(hasattr(User_1, "name"))
+
+	def test_str_output_user(self):
+		'''Tests for expected output'''
+		u1 = State()
+		output = f"[{u1.__class__.__name__}] ({u1.id}) {u1.__dict__}"
+		self.assertEqual(output, str(u1))
 
 
 if __name__ == "__main__":
