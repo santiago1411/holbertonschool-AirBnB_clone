17e279106d2fa312d7f942f913fcada7e1ce2b86
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-07-06 19:00:49 -0500

    state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -10,29 +10,35 @@ from models.base_model import BaseModel
 from models import state
 from models.state import State
 
+
 class Test_state(unittest.TestCase):
 
     """"""
+
+    def setUp(self):
+        """Sets up user instance"""
+        self.my_state = State()
 
     def test_is_an_instance_user(self):
         """user instatiating"""
         my_state = State()
         self.assertIsInstance(my_state, State)
-    
+
     def test_State_id(self):
         my_state = State()
         self.assertIsInstance(my_state.id, str)
         self.assertTrue(hasattr(my_state, "id"))
-    
+
     def test_created(self):
         my_state = State()
         self.assertTrue(type(my_state.created_at), str)
         self.assertTrue(hasattr(my_state, "created_at"))
-    
+
     def test_updated(self):
         my_state = State()
         self.assertTrue(type(my_state.updated_at), str)
         self.assertTrue(hasattr(my_state, "updated_at"))
+
 
 if __name__ == "__main__":
     unittest.main()
