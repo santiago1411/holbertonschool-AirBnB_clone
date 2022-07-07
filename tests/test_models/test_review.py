756514f74946da564cdb47516c274814dca5f270
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-07-06 19:27:24 -0500

    Amenit

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,38 +1,69 @@
 #!/usr/bin/python3
-
-"""
-unittest for user
-"""
-
-import models
-import unittest
-from models.base_model import BaseModel
-from models import review
+'''Unittest for review class'''
 from models.review import Review
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
 
-class Test_review(unittest.TestCase):
 
-    """"""
+class TestUserClass(unittest.TestCase):
 
-    def test_is_an_instance_user(self):
-        """user instatiating"""
-        my_review = Review()
-        self.assertIsInstance(my_review, Review)
-    
-    def test_review_id(self):
-        my_review = Review()
-        self.assertIsInstance(my_review.id, str)
-        self.assertTrue(hasattr(my_review, "id"))
-    
-    def test_created(self):
-        my_review = Review()
-        self.assertTrue(type(my_review.created_at), str)
-        self.assertTrue(hasattr(my_review, "created_at"))
-    
-    def test_updated(self):
-        my_review = Review()
-        self.assertTrue(type(my_review.updated_at), str)
-        self.assertTrue(hasattr(my_review, "updated_at"))
+    def setUp(self):
+        """Sets up user instance"""
+        self.User_1 = Review()
+
+    def test_type(self):
+        """test to check type of instance"""
+        self.assertEqual(type(self.User_1), Review)
+
+    def test_user_id(self):
+        """test to check user id"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.id), str)
+        self.assertTrue(hasattr(User_1, "id"))
+
+    def test_user_created(self):
+        """test to check user created_at"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.created_at), type(datetime.now()))
+        self.assertTrue(hasattr(User_1, "updated_at"))
+
+    def test_user_updated(self):
+        """test to check user updated_at"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.updated_at), type(datetime.now()))
+        self.assertTrue(hasattr(User_1, "updated_at"))
+
+    def test_user_email(self):
+        '''Tests email for User'''
+        u1 = Review()
+        self.assertEqual(type(u1.email), str)
+        self.assertTrue(hasattr(u1, "email"))
+
+    def test_user_password(self):
+        """test to check user password"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.password), str)
+        self.assertTrue(hasattr(User_1, "password"))
+
+    def test_user_first_name(self):
+        """test to check user first name"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.first_name), str)
+        self.assertTrue(hasattr(User_1, "first_name"))
+
+    def test_user_last_name(self):
+        """test to check user last_name"""
+        User_1 = Review()
+        self.assertEqual(type(User_1.last_name), str)
+        self.assertTrue(hasattr(User_1, "last_name"))
+
+    def test_str_output_user(self):
+        '''Tests for expected output'''
+        u1 = Review()
+        output = f"[{u1.__class__.__name__}] ({u1.id}) {u1.__dict__}"
+        self.assertEqual(output, str(u1))
+
 
 if __name__ == "__main__":
     unittest.main()
